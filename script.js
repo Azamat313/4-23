9d43ffd97a12cc382a534e36e9b20330a2ee7218
Aristik <aristiktop8@gmail.com>
2025-03-20 12:34:59 +0500

    3 tasks js

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
-"use strict";
+"use strong"
 
-// alert("Hello");
-// const con = confirm("Are you here?");
-// console.log(con);
+//first task
+let Time = 0;
+const TimeDisplay = document.getElementById('timeWeb');
 
-// const answer = +prompt("Вам есть 18?", 16);  
-// console.log(answer + 5);
+setInterval(() => {
+    Time++;
+    TimeDisplay.textContent = Time;
+}, 1000)
 
-// const mas = [];
+//second task
+const names = ['Kirill', 'Anya', 'Olya', 'Aristarh', 'Den'];
+const sorting = [...names].sort((a, b) => a.length - b.length);
+console.log(sorting);
 
-// mas[0] = prompt("СКолько вам лет?", '');
-// mas[1] = prompt("СКолько вам?", "");
-// mas[2] = prompt("как вас зовут?", "");
+//third task
+let ask = +prompt('Введите число для вычесления простого числа: ', "100");
+if (ask < 2) {
+    console.log('Простое число не может быть меньше 2');
+} else {
+    PrimeNumber = true;
 
-// const category = 'toyyys';
-// console.log(`https://someurl.com/${category}/5`)
+    for (let i = 2; i <= Math.sqrt(ask); i++) {
+        if (ask % 2 === 0) {
+            PrimeNumber = false;
+            break;
+        }
+    }
 
-const eur = 0.011;
-const usd = 0.012;
-const gbp = 0.0090;
-
-const request = +prompt("Enter rub to convert to eur, usd, gbp:", 100);
-
-let solutionEur = (request * eur).toFixed(4);
-let solutionUsd = (request * usd).toFixed(4);
-let solutionGbp = (request * gbp).toFixed(4);
-
-console.log(`Result: ${request}rub = ${solutionEur}eur, ${request}rub = ${solutionUsd}usd, ${request}rub = ${solutionGbp}gbp`);
+    if (PrimeNumber) {
+        console.log('Это простое число');
+    } else {
+        console.log('Это составное число');
+    }
+}
